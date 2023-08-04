42ca126faeaa5acc95e21241920a8ff76d962dcf
mhmmdputera <puteramhmmd@gmail.com>
2023-08-03 22:14:49 -0800

    UsersService

--- src/Services/postgres/UsersService.js
+++ src/Services/postgres/UsersService.js
@@ -1,8 +1,67 @@
 /* eslint-disable linebreak-style */
+const {nanoid} = require('nanoid');
 const {Pool} = require('pg');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
+// eslint-disable-next-line require-jsdoc
 class UsersService {
-    constructor() {
-      this._pool = new Pool();
+  // eslint-disable-next-line require-jsdoc
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  async addUser({username, password, fullname}) {
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length > 0) {
+      throw new InvariantError('User gagal ditambahkan');
     }
   }
+
+  // eslint-disable-next-line require-jsdoc
+  async getUserById(id) {
+    const query = {
+      text: 'SELECT * FROM users WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+}
+
+module.exports = UsersService;
