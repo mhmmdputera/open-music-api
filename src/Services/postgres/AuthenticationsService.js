cdb224f69fa7f71af25d4288687809474192bfd0
mhmmdputera <puteramhmmd@gmail.com>
2023-08-07 21:30:37 -0800

    AuthenticationService Completed

--- src/Services/postgres/AuthenticationsService.js
+++ src/Services/postgres/AuthenticationsService.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 const {Pool} = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 // eslint-disable-next-line require-jsdoc
 class AuthenticationsService {
@@ -12,6 +13,29 @@ class AuthenticationsService {
   async addRefreshToken(token) {
     const query = {
       text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  async verifyRefreshToken(token) {
+    const query = {
+      text: 'SELECT * FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new InvariantError('Refresh token tidak valid');
+    }
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  async deleteRefreshToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
       values: [token],
     };
 
